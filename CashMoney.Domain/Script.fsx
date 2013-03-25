ba8770be22eb11124ce8644335aa4e6102c46360
Matt Watson <mattwatson1@gmail.com>
2013-03-25 17:42:47 +0000

    Added function to write balances

--- CashMoney.Domain/Script.fsx
+++ CashMoney.Domain/Script.fsx
@@ -2,6 +2,8 @@
 open System
 open CashMoney.Domain
 
+type Direction = In | Out
+
 type AccountType = Asset | Liability | Income | Expense | Payable | Receivable | Equity
 
 type Account =
@@ -12,8 +14,6 @@ type Account =
 type Money = 
     | Amount of decimal
     | Fraction of (decimal * int)
-
-type Direction = In | Out
 
 type Transaction = 
     { Direction : Direction;
@@ -27,6 +27,11 @@ type Journal =
       Description   : string;
       Transactions  : Transaction list}
 
+let convertMoney a = 
+    match a with 
+    | Amount a -> a
+    | Fraction (a,f) ->  (a / decimal f)
+
 let bank = { Name = "Bank" ; Enabled = true ; Type = Asset }
 let cash = { Name = "Cash" ; Enabled = true ; Type = Asset }
 let food = { Name = "Food" ; Enabled = true ; Type = Expense }
@@ -34,10 +39,21 @@ let food = { Name = "Food" ; Enabled = true ; Type = Expense }
 let tran1 = { Direction = In ; Account = bank ; Amount = Amount 5.35M ; Note = "" ; Verified = false }
 let tran2 = { Direction = Out; Account = cash ; Amount = Fraction (5.34M, 3) ; Note = "" ; Verified = false }
 
-let journal1 = { 
+let journal1 = {
     Date = DateTime.Today ;
     Description = "Bought some food with Cash" 
     Transactions = 
         [{ Direction = Out ; Account = cash ; Amount = Amount 5.99M ; Note = "" ; Verified = false };
         { Direction = In ; Account = food ; Amount = Amount 5.99M ; Note = "Burger" ; Verified = false }]
 }
+
+let balances = 
+    journal1.Transactions
+    |> Seq.groupBy (fun t -> t.Account)
+    |> Seq.map (fun (a, ts) -> a, ts |> Seq.sumBy (fun t -> if t.Direction = In then convertMoney t.Amount else -convertMoney t.Amount))
+
+balances |> Seq.iter (fun (ac, am) -> printfn "%s:\t%M" ac.Name am)
+
+
+
+
