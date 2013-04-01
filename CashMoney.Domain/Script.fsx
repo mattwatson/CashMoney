798a4fd7fa5dd426a2eb15944e2d9f613ac2c214
Matt Watson <mattwatson1@gmail.com>
2013-04-01 20:50:38 +0100

    Added parsing from accounts.xml file

--- CashMoney.Domain/Script.fsx
+++ CashMoney.Domain/Script.fsx
@@ -1,63 +1,114 @@
 ﻿#load "Domain.fs"
 open System
 open CashMoney.Domain
+open System.Linq
+#r "System.Xml.Linq" //Don't understand why I need this - not even sure if I do.
+open System.Xml.Linq
 
-type Direction = In | Out
-
-type Money = 
-    | Amount of decimal
-    | Fraction of (decimal * int)
-
+//Domain
 type AccountType = Asset | Liability | Income | Expense | Payable | Receivable | Equity
 
 type Account =
-    { Id      : int;    //Id will never change
-      Name    : string; //want to support renaming, so either it's mutable or the collection of accounts will allow replacements
+    { Id      : int;
+      Name    : string;
       Enabled : bool;
-      Type    : AccountType; }
+      Type    : AccountType;
+      Tags    : int list }
 
-type Transaction = 
-    { Direction : Direction;
-      Account   : int;
-      Amount    : Money;
-      Note      : string;
-      Verified  : bool }
+//XML parsing functions
+let attribute (node:XElement, name) = 
+    let attr = node.Attribute(XName.Get(name))
+    if (attr <> null) then attr.Value else failwith("Could not find attribute called '" + name + "'")    
 
-type Journal = 
-    { Date          : DateTime;
-      Description   : string;
-      Transactions  : Transaction list}
+let intAttribute (node:XElement, name) = 
+    let attr = attribute(node, name)
+    let (ok, result) = Int32.TryParse (attr)
+    if (ok) then result else failwith("Unabled to parse int out of '" + attr + "' for attribute called '" + name + "'")
+
+let boolAttribute (node:XElement, name) = 
+    let attr = attribute(node, name)
+    let (ok, result) = Boolean.TryParse (attr)
+    if (ok) then result else failwith("Unabled to parse boolean out of '" + attr + "' for attribute called '" + name + "'")
+
+let parseAccountType(node) =
+    match intAttribute(node, "type") with
+    | 1 -> Asset
+    | 2 -> Liability
+    | 3 -> Income
+    | 4 -> Expense
+    | 5 -> Payable
+    | 6 -> Receivable
+    | 7 -> Equity
+    | x -> failwith ("Unknown Account Type specified '" + string x + "'")
+
+let parseAccountTags(node:XElement) =
+    let elements = node.Element(XName.Get("tags"))
+    node.Element(XName.Get("tags")).Elements()
+    |> List.ofSeq 
+    |> List.map (fun tag -> 
+        let (ok, result) = Int32.TryParse(tag.Value)
+        if ok then result else failwith ("Unabled to parse int from '" + tag.Value + "' for tag'"))
+
+let parseAccount (account :XElement) =
+    let id = intAttribute (account, "id")
+    let name = attribute (account, "name")
+    let accountType = parseAccountType account
+    let isEnabled = boolAttribute (account, "isEnabled")
+    let tags = parseAccountTags account
+    { Id = id; Name = name; Enabled = isEnabled; Type = accountType; Tags = tags}
+
+
+let accounts = XDocument.Load(@"C:\Users\Matt\Documents\GitHub\CashMoney\CashMoney\Data\accounts.xml").Root.Elements() 
+               |> List.ofSeq 
+               |> List.map parseAccount
+
+
+accounts |> List.sortBy (fun x -> x.Name) |> List.iter (fun a -> printfn "%s (%A) Tags: %A" a.Name a.Type a.Tags)
+
+//type Direction = In | Out
+//
+//type Money = 
+//    | Amount of decimal
+//    | Fraction of (decimal * int)
+
+//type Transaction = 
+//    { Direction : Direction;
+//      Account   : int;
+//      Amount    : Money;
+//      Note      : string;
+//      Verified  : bool }
+//
+//type Journal = 
+//    { Date          : DateTime;
+//      Description   : string;
+//      Transactions  : Transaction list}
 
 //Maybe when saving down the Journals to files they are saved to the Account of their first transaction.
 //Issues: when an account is renamed what happens to it's file? What if the Journal has no transactions?
 
 //Or could just save journals by the year, or by a number per file.
 
-let convertMoney a = 
-    match a with 
-    | Amount a -> a
-    | Fraction (a,f) ->  (a / decimal f)
+//let convertMoney a = 
+//    match a with 
+//    | Amount a -> a
+//    | Fraction (a,f) ->  (a / decimal f)
+//
 
-let bank = { Id = 1; Name = "Bank" ; Enabled = true ; Type = Asset }
-let cash = { Id = 2; Name = "Cash" ; Enabled = true ; Type = Asset }
-let food = { Id = 3; Name = "Food" ; Enabled = true ; Type = Expense }
+//let tran1 = { Direction = In ; Account = bank.Id ; Amount = Amount 5.35M ; Note = "" ; Verified = false }
+//let tran2 = { Direction = Out; Account = cash.Id ; Amount = Fraction (5.34M, 3) ; Note = "" ; Verified = false }
+//
+//let journal1 = {
+//    Date = DateTime.Today ;
+//    Description = "Bought some food with Cash" 
+//    Transactions = 
+//        [{ Direction = Out ; Account = cash.Id ; Amount = Amount 5.99M ; Note = "" ; Verified = false };
+//        { Direction = In ; Account = food.Id ; Amount = Amount 5.99M ; Note = "Burger" ; Verified = false }]
+//}
 
-let accounts = [ bank; cash; food ]
-
-let tran1 = { Direction = In ; Account = bank.Id ; Amount = Amount 5.35M ; Note = "" ; Verified = false }
-let tran2 = { Direction = Out; Account = cash.Id ; Amount = Fraction (5.34M, 3) ; Note = "" ; Verified = false }
-
-let journal1 = {
-    Date = DateTime.Today ;
-    Description = "Bought some food with Cash" 
-    Transactions = 
-        [{ Direction = Out ; Account = cash.Id ; Amount = Amount 5.99M ; Note = "" ; Verified = false };
-        { Direction = In ; Account = food.Id ; Amount = Amount 5.99M ; Note = "Burger" ; Verified = false }]
-}
-
-let balances = 
-    journal1.Transactions
-    |> Seq.groupBy (fun t -> t.Account)
-    |> Seq.map (fun (a, ts) -> a, ts |> Seq.sumBy (fun t -> if t.Direction = In then convertMoney t.Amount else -convertMoney t.Amount))
-
-balances |> Seq.iter (fun (ac, am) -> printfn "%s:\t%M" (accounts |> List.filter (fun f -> f.Id = ac) |> List.head).Name am)
+//let balances = 
+//    journal1.Transactions
+//    |> Seq.groupBy (fun t -> t.Account)
+//    |> Seq.map (fun (a, ts) -> a, ts |> Seq.sumBy (fun t -> if t.Direction = In then convertMoney t.Amount else -convertMoney t.Amount))
+//
+//balances |> Seq.iter (fun (ac, am) -> printfn "%s:\t%M" (accounts |> List.filter (fun f -> f.Id = ac) |> List.head).Name am)
+//
